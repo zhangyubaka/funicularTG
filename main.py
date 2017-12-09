8d4697e91fd03fb47d755e08b05abff42fbda135
zhangyubaka <zhangyubaka@gmail.com>
2017-12-10 00:13:14 +0800

    things occurred

--- main.py
+++ main.py
@@ -9,46 +9,63 @@ import socks
 from pprint import pprint
 import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+authLog = logging.StreamHandler()
+authLog.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+authLog.setFormatter(formatter)
+logger.addHandler(authLog)
+
 
 async def auth() -> tuple:
-    authLog = logging.StreamHandler()
-    authLog.setLevel(logging.DEBUG)
     if os.path.isfile(config.session):
         client = telethon.TelegramClient(config.session)
         if client.is_connected():
+            logger.debug("Assuming client is connected and good to go, returning.")
             me = await client.get_me()
             return me, client
         else:
             raise Exception('Auth Failed')
     else:
         if config.proxy:
-            client = telethon.TelegramClient(config.session, config.api_id, config.api_hash,
-                                             (socks.SOCKS5, config.proxyHost, config.proxyPort))
+            logger.debug("Connecting with proxy...")
+            client = telethon.TelegramClient(session=config.session, api_id=config.api_id, api_hash=config.api_hash,
+                                             proxy=(socks.SOCKS5, config.proxyHost, config.proxyPort))
         else:
-            client = telethon.TelegramClient(config.session, config.api_id, config.api_hash,
-                                             (socks.SOCKS5, config.proxyHost, config.proxyPort))
+            logger.debug("Connecting...")
+            client = telethon.TelegramClient(config.session, config.api_id, config.api_hash)
         await client.connect()
         if client.is_connected():
+            logger.debug("Sending auth code...")
             await client.send_code_request(config.phone_number)
+            logger.debug("Code sent!")
             me = await client.sign_in(config.phone_number, input('Enter auth code: '))
             return me, client
         else:
             raise Exception('Auth Failed')
 
 
-async def get_dialogs(client):
-    #dialogs = await client.get_dialogs(limit=None)
+async def get_dialogs(client) -> tuple:
+    # dialogs = await client.get_dialogs(limit=None)
+    logger.debug("Getting dialogs...")
+    dialogs, entities = await client.get_dialogs()
+    return dialogs, entities
 
-    dialogs = await client.get_dialogs()
-    return dialogs
+
+async def get_history(client, entries, offset_id=0):
+    history = []
+    for i in entries:
+        logger.debug("Getting history for id " + i.id)
+        history.append(await client.get_message_history(entity=i, offset_id=offset_id))
+    return history
 
 
 async def main():
     me, client = await auth()
-    pprint(get_dialogs(client))
+    
 
 if __name__ == '__main__':
-    logging.getLogger(__name__)
     loop = uvloop.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(main())
